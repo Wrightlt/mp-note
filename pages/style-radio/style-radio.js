f9ef54810541f2da6ecb86b1ea6bf6ca38f70133
Wrightlt <wright8140@gmail.com>
2019-03-28 17:06:28 +0800

    first commit

---
+++ pages/style-radio/style-radio.js
@@ -0,0 +1,78 @@
+const payTypes = [{
+  type: '微信支付',
+  icon: 'ic_wechatpay@3x.png',
+  checked: 'true'
+},
+{
+  type: '余额支付',
+  icon: 'ic_balance@3x.png',
+  notice: '当前余额 6539 元'
+}
+]
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    payTypes: payTypes
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
